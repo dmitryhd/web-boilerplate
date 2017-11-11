4480ae430e0c757a78a09db5d977b379b68fd740
Dmitry Khodakov <dkhodakov@avito.ru>
2017-11-11 16:16:47 +0300

    playing with vue

--- js/script.js
+++ js/script.js
@@ -1,6 +1,41 @@
 window.App = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue.js!'
-    }
+        message: 'Config editor',
+        sampleConfig: {
+            param1: true,
+            param2: 'hello',
+            param3: 1.1,
+            weights: {
+                w1: 0.1,
+                w2: 0.2,
+                w3: 0.3,
+                w4: 0.3,
+            },
+        },
+        configToRender: []
+    },
+    methods: {
+        sendConfig: function() {
+            console.log(JSON.stringify(this.sampleConfig));
+        },
+        renderConfig: function() {
+            var p = this.sampleConfig;
+            this.configToRender = [];
+            for (var key in p) {
+                if (p.hasOwnProperty(key)) {
+                    this.configToRender.push({
+                        name: key,
+                        value: p[key],
+                        type: typeof p[key],
+                    })
+                }
+            }
+            this.sendConfig();
+        }
+    },
+    mounted: function() {
+        this.renderConfig();
+    },
 })
+
