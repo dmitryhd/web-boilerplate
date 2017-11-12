0eb77e9bd87760f3eef41af3e7ff91e084b15a2a
Dmitry Khodakov <dkhodakov@avito.ru>
2017-11-12 16:02:43 +0300

    now with component

--- js/script.js
+++ js/script.js
@@ -1,3 +1,47 @@
+
+Vue.component('config-editor', {
+    template: `
+    <table class="table small-table col sm-6">
+    <thead></thead>
+    <tbody>
+        <tr v-for='(value, key) in config'>
+            <td class=no-padding>{{ key }}</td>
+            <td class=no-padding>
+                <span v-if="typeof config[key] == 'boolean'">
+                    <input type="checkbox" id="checkbox" v-model="config[key]">
+                    <label for="checkbox">{{ config[key] }}</label>
+                </span>
+                <span v-if="typeof config[key] == 'string'">
+                    <input v-model="config[key]">
+                </span>
+                <span v-if="typeof config[key] == 'number'">
+                    <input v-model.number="config[key]" type="number">
+                </span>
+
+                <span v-if="typeof config[key] == 'object'">
+                    <!-- {{ config[key] }} -->
+
+                    <table>
+                        <tr v-for="(innerValue, innerKey) in config[key]">
+                            <td class=no-padding>
+                                {{ innerKey}}
+                            </td>
+                            <td class=no-padding>
+                                <span v-if="typeof config[key][innerKey] == 'number'">
+                                    <input v-model.number="config[key][innerKey]" type="number">
+                                </span>
+                            </td>
+                        </tr>
+                    </table>
+                </span>
+            </td>
+        </tr>
+    </tbody>
+</table>
+    `,
+    props: ['config'],
+})
+
 window.App = new Vue({
     el: '#app',
     data: {
