50e60c42f367ae84e0a715a5fa09ee81577122a5
Dmitry Khodakov <dkhodakov@avito.ru>
2017-11-16 23:16:30 +0300

    attrs go

--- js/attribute_mapper.js
+++ js/attribute_mapper.js
@@ -1,85 +1,52 @@
 
-Vue.component('config-editor', {
-    template: `
-    <table class="table small-table col sm-6">
-    <thead></thead>
-    <tbody>
-        <tr v-for='(value, key) in config'>
-            <td class=no-padding>{{ key }}</td>
-            <td class=no-padding>
-                <span v-if="typeof config[key] == 'boolean'">
-                    <input type="checkbox" id="checkbox" v-model="config[key]">
-                    <label for="checkbox">{{ config[key] }}</label>
-                </span>
-                <span v-if="typeof config[key] == 'string'">
-                    <input v-model="config[key]">
-                </span>
-                <span v-if="typeof config[key] == 'number'">
-                    <input v-model.number="config[key]" type="number">
-                </span>
-
-                <span v-if="typeof config[key] == 'object'">
-                    <!-- {{ config[key] }} -->
-
-                    <table>
-                        <tr v-for="(innerValue, innerKey) in config[key]">
-                            <td class=no-padding>
-                                {{ innerKey}}
-                            </td>
-                            <td class=no-padding>
-                                <span v-if="typeof config[key][innerKey] == 'number'">
-                                    <input v-model.number="config[key][innerKey]" type="number">
-                                </span>
-                            </td>
-                        </tr>
-                    </table>
-                </span>
-            </td>
-        </tr>
-    </tbody>
-</table>
-    `,
-    props: ['config'],
-})
+function byId(element, index, array) {
+    return
+    return element > 1;
+}
 
 window.App = new Vue({
     el: '#app',
     data: {
-        message: 'Config editor',
-        sampleConfig: {
-            param1: true,
-            param2: 'hello',
-            param3: 1.1,
-            weights: {
-                w1: 0.1,
-                w2: 0.2,
-                w3: 0.3,
-                w4: 0.3,
-            },
-        },
-        configToRender: []
+        goodList: [],
+        badList: [],
+        initialWords: [
+            {'word': 'a', 'id': 0},
+            {'word': 'b', 'id': 1},
+            {'word': 'b', 'id': 2},
+            {'word': 'b', 'id': 3},
+            {'word': 'b', 'id': 4},
+            {'word': 'b', 'id': 5},
+            {'word': 'b', 'id': 6},
+        ],
     },
     methods: {
         sendConfig: function() {
-            console.log(JSON.stringify(this.sampleConfig));
+            console.log(JSON.stringify({
+                good: this.goodList,
+                bad: this.badList,
+            }));
         },
-        renderConfig: function() {
-            var p = this.sampleConfig;
-            this.configToRender = [];
-            for (var key in p) {
-                if (p.hasOwnProperty(key)) {
-                    this.configToRender.push({
-                        name: key,
-                        value: p[key],
-                        type: typeof p[key],
-                    })
-                }
-            }
-            this.sendConfig();
+        modeWordTo: function(id, to) {
+            // remove
+            idx = this.initialWords.findIndex(
+                function(element, index, array){
+                    if (element.id == id) {
+                        return true;
+                    }
+                    return false;
+                });
+            obj = this.initialWords[idx];
+            console.log('To ' + to + ' ' + obj + ', id = ' + id);
+            this.initialWords.splice(idx, 1);
+            if (to == 'good') {
+                this.goodList.push(obj);
+            } else {
+                this.badList.push(obj);
+            };
         }
     },
     mounted: function() {
-        this.renderConfig();
+        console.log('gogo');
     },
 })
 
